68107489d97437686076b471d10aed572b9b24ed
김민주 <w2023@e-mirim.hs.kr>
2021-07-27 14:32:17 +0900

    Edit diaryActivity.java 화면보이기 성공

--- app/src/main/java/kr/hs/emirim/w2023/secret_daiary/DiaryActivity.java
+++ app/src/main/java/kr/hs/emirim/w2023/secret_daiary/DiaryActivity.java
@@ -1,4 +1,16 @@
 package kr.hs.emirim.w2023.secret_daiary;
 
-public class DiaryActivity {
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diary_activity);
+    }
 }
